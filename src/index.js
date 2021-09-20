843fcbaf944ec7b7003774500c918477a86f3d92
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2021-09-20 16:35:08 +0300

    hw 10 revisions

--- src/index.js
+++ src/index.js
@@ -2,53 +2,39 @@ import menuCardsTamplates from '../src/templates/menu-cards.hbs'
 import './sass/main.scss';
 import menu from './menu.json'
 
-
 const cardsContainer = document.querySelector('.js-menu');
 const cardsMarkup = menuCardsTamplates(menu);
-
-cardsContainer.insertAdjacentHTML('beforeend', cardsMarkup);
-
-const bodyEl = document.querySelector('body');
-const switchEl = document.querySelector('#theme-switch-toggle');
-
-//console.log(switchEl)
-
-bodyEl.addEventListener('change', onClassList);
-switchEl.addEventListener('change', onLocalStorage);
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+const bodyEl = document.body;
+const switchEl = document.querySelector('#theme-switch-toggle');
+
+cardsContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+bodyEl.addEventListener('change', onClassList);
+switchEl.addEventListener('change', onClassList);
+
 
 function onClassList(evt) {
-    
-    const checkStatus = switchEl.checked;
-    
-    if (checkStatus) {
-      bodyEl.classList.add(Theme.DARK);
-      bodyEl.classList.remove(Theme.LIGHT)
-    } else {
-      bodyEl.classList.add(Theme.LIGHT);
-      bodyEl.classList.remove(Theme.DARK);
-    }
-    
-}
-
-function onLocalStorage(evt) {
-    const checkStatus = switchEl.checked;
-
-    if (checkStatus) {
-        localStorage.setItem('theme',Theme.DARK)
+    if(evt.target.checked){
+    bodyEl.classList.add(Theme.DARK);
+    localStorage.setItem('theme',Theme.DARK)
+    bodyEl.classList.remove(Theme.LIGHT)
     }
     else {
-        localStorage.removeItem('theme',Theme.DARK);
-        localStorage.setItem('theme',Theme.LIGHT)
+    bodyEl.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme',Theme.LIGHT)
+    bodyEl.classList.remove(Theme.DARK);
     }
 }
 
-const themeInTime = localStorage.getItem('theme');
 
+
+const themeInTime = localStorage.getItem('theme');
 //console.log(themeInTime)
 
     if (themeInTime === Theme.DARK) {
